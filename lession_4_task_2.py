9ab684e19ce0bdfc8b544bf447dbbacd96f19dd0
Dmitry V. Kadnikov <73481082+dkadnikov@users.noreply.github.com>
2020-11-27 21:35:23 +0500

    Update lession_4_task_2.py
    #ДЛЯ ПРЕПОДОВАТЕЛЯ
    #ОБСУЖДАЛИ РЕШЕНИЕ С ОДНОКУРСКНИОМ И ОН СПРОСИЛ КАК РЕШИТЬ ЧЕРЕЗ ГЕНЕРАТОР ПРО УСЛОВИЮ, ТУТ МЕНЯ ОСЕНИЛО, ЧТО Я САМ СДЕЛАЛ БЕЗ YIELD
    # ПО ЭТОМУ ВОТ ВТОРОЙ ВАРИАНТ РЕШЕНИЯ

--- lession_4_task_2.py
+++ lession_4_task_2.py
@@ -18,3 +18,22 @@ my_list = [3, 20, 9, 5, 1, 8, 11, 6, 15]
 print(my_list)
 my_list_new = [el for el in my_list if el > my_list[my_list.index(el)-1]]
 print(my_list_new)
+
+
+#ДЛЯ ПРЕПОДОВАТЕЛЯ
+#ОБСУЖДАЛИ РЕШЕНИЕ С ОДНОКУРСКНИОМ И ОН СПРОСИЛ КАК РЕШИТЬ ЧЕРЕЗ ГЕНЕРАТОР ПРО УСЛОВИЮ, ТУТ МЕНЯ ОСЕНИЛО, ЧТО Я САМ СДЕЛАЛ БЕЗ YIELD
+# ПО ЭТОМУ ВОТ ВТОРОЙ ВАРИАНТ РЕШЕНИЯ
+# Решение-задачи-2 (ВАРИАНТ 2)
+print("Решение-задачи-2 (ВАРИАНТ №2)")
+print("")
+
+def countbugnum(iterable: list):
+    temp = iterable[0]
+    for i in iterable:
+        if i > temp:
+            yield i
+        temp = i
+if __name__ == '__main__':
+    source_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+    generator = countbugnum(source_list)
+    print(list(generator))
